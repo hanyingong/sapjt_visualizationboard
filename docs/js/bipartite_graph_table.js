d8a517eb621c7827eba0969618afd99a1426d540
hanyingong <hanying.ong@outlook.com>
2017-07-21 04:41:55 +0800

    version 2- complete bipartite graph

--- docs/js/bipartite_graph_table.js
+++ docs/js/bipartite_graph_table.js
@@ -1,58 +1,53 @@
 function draw_bipartite_table() {
+
   var bartite_datatable = 'dataset/bipartite_table_final.csv';
-  d3.csv(bartite_datatable, function(data2) {
-      //uncomment the statement below to convert it into array
-      data2 = data2.map(function(data2) {return [data2["new_venue_type"], data2["spe_clus_id"], data2["venue_name"], +data2["med_senti_score"]];});   //Once loaded, copy to dataset.
-      console.log(data2);
 
-      // capture restaurant label for filter
-      $("[class*=label]").mouseover(function() {
-          var selected_label =  $(event.target).text();
-          console.log(selected_label);
+  //# d3.csvParseRows(string[, row]) <> Equivalent to dsvFormat(",").parseRows.
 
-      // create cross filter based on label
-    //  var filterdataset = crossfilter(data2);
-    //  var tablefilter = filterdataset.dimension(function(data2) {return data2.new_venue_type;})
-    //      tablefilter.filter(selected_label);
-          //tablescore = filterdataset.dimension(function(data2) {return Math.floor(data2.med_senti_score);}),
-          //tablefilter = filterdataset.dimension(function(data2) {return data2.new_venue_type;}),
-          //filterType = tablescore.group().reduceSum(function(data2) {return +data2.med_senti_score}),
-          //filterTable = tablescore.groupAll().reduceCount();
+    d3.csv(bartite_datatable, function(data2) {
+    //var parsedCSV = d3.dsvFormat(",").parseRows(data2);         //var parsedCSV = d3.csvparseRows(data2);
+    data2 = data2.map(function(data2)
+            {return [data2["venue_name"], data2["spe_clus_id"],
+              +data2["med_senti_score"], data2["new_venue_type"]];});   //Once loaded, copy to dataset.
 
-      //testing - uncomment to do testing, view results in console
-    //  var totalCount = filterdataset.groupAll().reduceCount().value();
-    //  console.log(totalCount)
+    var container = d3.select('#bipartite_table')
+                      .append("table")
+                        .selectAll("tr")
+                            .data(data2).enter()
+                            .append("tr")
+                        .selectAll("td")
+                            .data(function(d) { return d; }).enter()
+                            .append("td")
+                            .text(function(d) { return d; });
 
-    //construct table
-    $(document).ready(function() {
-        $('#myTable').DataTable( {
-            data: data2,
-            columns: [
-                { title: "new_venue_type"},
-                { title: "Position" },
-                { title: "Office" },
-                { title: "med_senti_score" }
-            ]
-        } );
-    } );
+    console.log(data2)
 
-      //  retrieve table
-      $(document).ready(function($) {
+    $("[class*=label]").mouseover(function() {
+      var selected_label =  $(event.target).text();
+      console.log(selected_label);
+      });
+
+    // filter table
+    $(document).ready(function($) {
+      $('table').hide();
+
+      $("[class*=label]").mouseout(function() {
         $('table').hide();
-        $('#mySelector').change(function() {
-          $('table').show();
-          var selection = $selected_label;
-          var dataset = $('#myTable tbody').find('tr');
-          // show all rows first
-          dataset.show();
-          // filter the rows that should be hidden
-          dataset.filter(function(index, item) {
-            return $(item).find('td:first-child').text().split(',').indexOf(selection) === -1;
-          }).hide();
 
-        });
-      });
+      $("[class*=label]").mouseover(function() {
+        $('table').show();
+        var selected_label =  $(event.target).text();
+        var selection = selected_label;
+        var dataset = $('table').find('tr');
+        // show all rows first
+        dataset.show();
+        // filter the rows that should be hidden
+        dataset.filter(function(index, item) {
+          return $(item).find('td:nth-child(4)').text().split(',').indexOf(selection) === -1;
+        }).hide();
 
       });
+    });
+  });
   });
 }
